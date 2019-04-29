8002da7e2d11e9b368de9869e4108ee0c9d8dfab
Ollie Cahill <ollie@deveire.com>
2019-04-29 22:15:23 +0100

    Update setup-readymade.sh

--- setup-readymade.sh
+++ setup-readymade.sh
@@ -19,10 +19,24 @@ JAVA_DIR="OpenJDK8U-jre_x64_linux_hotspot_8u212b03"
 JAVA_EXPDIR="jdk8u212-b03-jre"
 JAVA_EXT=".tar.gz"
 
-command_exists () {
+cmd_exists () {
 	if [[ "$OSTYPE" == "linux-gnu" ]]; then
         hash "$1" 2>/dev/null || { echo >&2 "Installing $1 ..."; apt-get install --assume-yes "$1"; }
 	fi
+}
+
+cmd_wgetWithRetries () {
+	while sleep 1;
+	do
+        wget -O $2 $1 >& /dev/null
+        if( test $? -eq 0 ) then
+                echo "Got " +$1
+                break;
+        else
+                echo "Retrying for " + $1
+        fi
+	done
+
 }
 
 export DEBIAN_FRONTEND=noninteractive
@@ -30,23 +44,23 @@ export DEBIAN_FRONTEND=noninteractive
 apt update
 
 #  Check for required binaries
-command_exists "wget"
-sleep 3
-command_exists "mysql-server"
-sleep 3
-command_exists "nginx"
-sleep 3
+cmd_exists "wget"
+sleep 1
+cmd_exists "mysql-server"
+sleep 1
+cmd_exists "nginx"
+sleep 1
 
 cd /opt
 
 systemctl start nginx.service
 
 #Start nginx first so users get status page while env is getting setup
-sleep 2
+sleep 1
 unlink /etc/nginx/sites-enabled/default
-wget https://raw.githubusercontent.com/DevEire/configs/master/nginx-waiting.conf
-wget https://raw.githubusercontent.com/DevEire/configs/master/readymade-building.html
-mv readymade-building.html /usr/share/nginx/html/readymade-building.html
+cmd_wgetWithRetries https://raw.githubusercontent.com/DevEire/configs/master/nginx-waiting.conf nginx-waiting.conf
+cmd_wgetWithRetries https://raw.githubusercontent.com/DevEire/configs/master/readymade-building.html readymade-building.html
+mv  readymade-building.html /usr/share/nginx/html/readymade-building.html
 mv nginx-waiting.conf /etc/nginx/sites-enabled/nginx.conf
 
 systemctl restart nginx.service
@@ -57,8 +71,8 @@ mkdir ${PROJECT_TOP_LEVEL}
 cd ${PROJECT_TOP_LEVEL}
 
 echo "Geting and extract server binaries"
-wget ${TOMCAT_PATH}${TOMCAT_DIR}${TOMCAT_EXT}
-wget ${SOLR_PATH}${SOLR_DIR}${SOLR_EXT}
+cmd_wgetWithRetries ${TOMCAT_PATH}${TOMCAT_DIR}${TOMCAT_EXT} ${TOMCAT_DIR}${TOMCAT_EXT}
+cmd_wgetWithRetries ${SOLR_PATH}${SOLR_DIR}${SOLR_EXT} ${SOLR_DIR}${SOLR_EXT}
 
 echo "Extracting server binaries"
 tar -xvzf ${TOMCAT_DIR}${TOMCAT_EXT}
@@ -66,7 +80,7 @@ tar -xvzf ${SOLR_DIR}${SOLR_EXT}
 
 
 echo "Download Java8"
-wget ${JAVA_PATH}${JAVA_DIR}${JAVA_EXT}
+cmd_wgetWithRetries ${JAVA_PATH}${JAVA_DIR}${JAVA_EXT} ${JAVA_DIR}${JAVA_EXT}
 tar -xvzf ${JAVA_DIR}${JAVA_EXT}
 ln -s  ${JAVA_EXPDIR} "java"
 export JAVA_HOME="/opt/BRIGHTSPOT/java/"
@@ -78,14 +92,14 @@ cp -rv "${SOLR_DIR}/example/lib/ext/"* "${TOMCAT_DIR}/lib"
 cp "${SOLR_DIR}/dist/${SOLR_DIR}.war" "${TOMCAT_DIR}/webapps/solr.war"
 
 echo "Download and install Solr config"
-wget https://raw.githubusercontent.com/perfectsense/dari/master/etc/solr/config-5.xml
-wget https://raw.githubusercontent.com/perfectsense/dari/master/etc/solr/schema-12.xml
+cmd_wgetWithRetries https://raw.githubusercontent.com/perfectsense/dari/master/etc/solr/config-5.xml config-5.xml
+cmd_wgetWithRetries https://raw.githubusercontent.com/perfectsense/dari/master/etc/solr/schema-12.xml schema-12.xml
 mv config-5.xml "${TOMCAT_DIR}/solr/collection1/conf/solrconfig.xml"
 mv schema-12.xml "${TOMCAT_DIR}/solr/collection1/conf/schema.xml"
 
 echo "Using bare bones tomcat config.."
-wget https://raw.githubusercontent.com/DevEire/configs/master/context.xml
-wget https://raw.githubusercontent.com/DevEire/configs/master/server.xml
+cmd_wgetWithRetries https://raw.githubusercontent.com/DevEire/configs/master/context.xml context.xml
+cmd_wgetWithRetries https://raw.githubusercontent.com/DevEire/configs/master/server.xml server.xml
 
 rm -rf "${TOMCAT_DIR}/conf/server.xml"
 rm -rf "${TOMCAT_DIR}/conf/context.xml"
@@ -94,14 +108,13 @@ mv context.xml "${TOMCAT_DIR}/conf/context.xml"
 
 
 echo "Getting Mysql driver..."
-wget "https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.40.tar.gz"
+cmd_wgetWithRetries https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.40.tar.gz mysql-connector-java-5.1.40.tar.gz 
 tar -xvzf mysql-connector-java-5.1.40.tar.gz
 cp "mysql-connector-java-5.1.40/mysql-connector-java-5.1.40-bin.jar" "${TOMCAT_DIR}/lib"
 rm -f mysql-connector-java-5.1.40.tar.gz
 rm -rf mysql-connector-java-5.1.40
 
 echo "Create local database"
-
 if [[ "$MYSQL_PASS" -ne "" ]]; then
 	echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DB}" | /usr/bin/mysql "-u$MYSQL_USER" "-p$MYSQL_PASS"
 else
@@ -116,13 +129,11 @@ echo "Clean up.."
 rm -f ${TOMCAT_DIR}${TOMCAT_EXT}
 rm -f ${SOLR_DIR}${SOLR_EXT}
 
-
 echo "Start tomcat.."
 ${TOMCAT_DIR}/bin/startup.sh
 
 
 sleep 5
-
 while sleep 1;
 do
         wget -O - http://127.0.0.1:8080/ >& /dev/null
@@ -130,13 +141,12 @@ do
                 echo "Tomcat Running"
                 break;
         else
-                echo "Tomcat Starting up..."
-                break
+                echo "Tomcat Still Starting up..."
         fi
 done
 sleep 2
 
-wget https://raw.githubusercontent.com/DevEire/configs/master/nginx.conf
+cmd_wgetWithRetries https://raw.githubusercontent.com/DevEire/configs/master/nginx.conf nginx.conf
 mv nginx.conf /etc/nginx/sites-enabled/nginx.conf
 systemctl restart nginx.service
 
